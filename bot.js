7cc7882a461fbe3089c800d774917cc5dfb684fc
Tyler Warren <warrejt@yahoo.com>
2016-12-24 11:38:12 -0600

    initialized favorite bot; just need need to launch with node now!

--- bot.js
+++ bot.js
@@ -1,18 +1,19 @@
+//Dependencies
 var twit = require(’twit’);
 var config = require(’./config.js’);
-
-
 var Twitter = new twit(config);
 
-var retweet = function() {
-  var params = {
-    q: '#sanantoniospurs, #SpursWin, #spurs, #GoSpursGo, #InPopWeTrust',
-    result_type: 'recent',
-    lang: 'en'    
-  } 
+//Retweet Bot ----------------------------------
 
-  Twitter.get('search/tweets', params, function(err, data) {
-      // if there no errors
+var retweet = function() {
+  	var params = {
+	    q: '#sanantoniospurs, #SpursWin, #spurs, #GoSpursGo, #InPopWeTrust',
+	    result_type: 'recent',
+	    lang: 'en'    
+  	} 
+
+	Twitter.get('search/tweets', params, function(err, data) {
+      	// if there no errors
         if (!err) {
           // grab ID of tweet to retweet
             var retweetId = data.statuses[0].id_str;
@@ -35,3 +36,49 @@ var retweet = function() {
         }
     });
 }
+// grab & retweet as soon as program is running...
+retweet();
+// retweet in every 50 minutes
+setInterval(retweet, 3000000);
+
+//===================================================
+
+
+// Favorite Bot ------------------------------------
+
+// find a random tweet and 'favorite' it
+var favoriteTweet = function(){
+  	var params = {
+      	q: '#sanantoniospurs, #SpursWin, #spurs, #GoSpursGo, #InPopWeTrust',  // REQUIRED
+      	result_type: 'recent',
+      	lang: 'en'
+  	}
+
+  	// find the tweet
+  	Twitter.get('search/tweets', params, function(err,data){
+
+	    // find tweets
+	    var tweet = data.statuses;
+	    var randomTweet = ranDom(tweet);   // pick a random tweet
+
+	    // if random tweet exists
+	    if(typeof randomTweet != 'undefined'){
+	      	// Tell TWITTER to 'favorite'
+	      	Twitter.post('favorites/create', {id: randomTweet.id_str}, function(err, response)s{
+		        // if there was an error while 'favorite'
+		        if(err){
+		          console.log('CANNOT BE FAVORITE... Error');
+		        }
+		        else {
+		          console.log('FAVORITED... Success!!!');
+	        	}
+	    	});
+	    }
+  	});
+}
+
+// grab & favorite as soon as program is running...
+favoriteTweet();
+// favorite a tweet in every 60 minutes
+setInterval(favoriteTweet, 3600000);
+
