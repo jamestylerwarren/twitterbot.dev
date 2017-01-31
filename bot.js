0ac932751452d02cae4655d8e3b071ff4d4457e3
Tyler Warren <warrejt@yahoo.com>
2017-01-30 20:59:18 -0600

    changing bot searches for coach pop and putting everything under a different twitter handle

--- bot.js
+++ bot.js
@@ -6,40 +6,40 @@ var Twitter = new twit(config);
 //Retweet Bot ----------------------------------
 
 var retweet = function() {
-  	var params = {
-	    q: '#sanantoniospurs OR #SpursWin OR #GoSpursGo OR #InPopWeTrust',
-	    result_type: 'recent',
-	    lang: 'en',    
-  	} 
+	var params = {
+    q: '#popquotes OR #greggpopovich OR #PopQuotes OR #GreggPopovich OR #inpopwetrust OR #InPopWeTrust OR #WisdomOfCoachPop OR #PopForPrez OR #PresidentPop OR #CoachPoppa OR #ILikeItWhenYouCallMeCoachPoppa OR #CoachPoppa OR #PopForPresident OR #PopBeingPop OR #Pop2020',
+    result_type: 'recent',
+    lang: 'en',    
+	} 
 
-	Twitter.get('search/tweets', params, function(err, data) {
-      	// if there no errors
-        if (!err) {
-          // grab ID of tweet to retweet
-            var retweetId = data.statuses[0].id_str;
-            // Tell TWITTER to retweet
-            Twitter.post('statuses/retweet/:id', {
-                id: retweetId
-            }, function(err, response) {
-                if (response) {
-                    console.log('Retweeted!!!');
-                }
-                // if there was an error while tweeting
-                if (err) {
-                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
-                }
-            });
-        }
-        // if unable to Search a tweet
-        else {
-          console.log('Something went wrong while SEARCHING...');
-        }
-    });
+  Twitter.get('search/tweets', params, function(err, data) {
+  	// if there no errors
+    if (!err) {
+      // grab ID of tweet to retweet
+        var retweetId = data.statuses[0].id_str;
+        // Tell TWITTER to retweet
+        Twitter.post('statuses/retweet/:id', {
+            id: retweetId
+        }, function(err, response) {
+            if (response) {
+                console.log('Retweeted!!!');
+            }
+            // if there was an error while tweeting
+            if (err) {
+                console.log('Something went wrong while RETWEETING... Duplication maybe...');
+            }
+        });
+    }
+    // if unable to Search a tweet
+    else {
+      console.log('Something went wrong while SEARCHING...');
+    }
+  });
 }
 // grab & retweet as soon as program is running...
 retweet();
-// retweet in every 50 minutes
-setInterval(retweet, 3000000);
+// retweet in every 30 minutes
+setInterval(retweet, 1800000);
 
 //===================================================
 
@@ -48,39 +48,39 @@ setInterval(retweet, 3000000);
 
 // find a random tweet and 'favorite' it
 var favoriteTweet = function(){
-  	var params = {
-      	q: '#sanantoniospurs OR #SpursWin OR #GoSpursGo OR #InPopWeTrust',  
-      	result_type: 'recent',
-      	lang: 'en',
-  	}
+	var params = {
+    	q: '#popquotes OR #greggpopovich OR #PopQuotes OR #GreggPopovich OR #inpopwetrust OR #InPopWeTrust OR #WisdomOfCoachPop OR #PopForPrez OR #PresidentPop OR #CoachPoppa OR #ILikeItWhenYouCallMeCoachPoppa OR #CoachPoppa OR #PopForPresident OR #PopBeingPop OR #Pop2020',  
+    	result_type: 'recent',
+    	lang: 'en',
+	}
 
-  	// find the tweet
-  	Twitter.get('search/tweets', params, function(err,data){
+	// find the tweet
+	Twitter.get('search/tweets', params, function(err,data){
 
-	    // find tweets
-	    var tweet = data.statuses;
-	    var randomTweet = ranDom(tweet);   // pick a random tweet
+    // find tweets
+    var tweet = data.statuses;
+    var randomTweet = ranDom(tweet);   // pick a random tweet
 
-	    // if random tweet exists
-	    if(typeof randomTweet != 'undefined'){
-	      	// Tell TWITTER to 'favorite'
-	      	Twitter.post('favorites/create', {id: randomTweet.id_str}, function(err, response) {
-		        // if there was an error while 'favorite'
-		        if(err){
-		          console.log('CANNOT BE FAVORITE... Error');
-		        }
-		        else {
-		          console.log('FAVORITED... Success!!!');
-	        	}
-	    	});
-	    }
-  	});
+    // if random tweet exists
+    if(typeof randomTweet != 'undefined'){
+      	// Tell TWITTER to 'favorite'
+      	Twitter.post('favorites/create', {id: randomTweet.id_str}, function(err, response) {
+	        // if there was an error while 'favorite'
+	        if(err){
+	          console.log('CANNOT BE FAVORITE... Error');
+	        }
+	        else {
+	          console.log('FAVORITED... Success!!!');
+        	}
+    	});
+    }
+	});
 }
 
 // grab & favorite as soon as program is running...
 favoriteTweet();
-// favorite a tweet in every 60 minutes
-setInterval(favoriteTweet, 3600000);
+// favorite a tweet in every 20 minutes
+setInterval(favoriteTweet, 1200000);
 
 
 // function to generate a random tweet 
