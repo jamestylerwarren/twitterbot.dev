b98011049526df8890ceaaa2b962ecea0b80968a
Tyler Warren <warrejt@yahoo.com>
2017-06-11 15:07:27 -0500

    fix require for config vars

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 //Dependencies
 var twit = require('twit');
-var config = require('config.js');
+var config = require('./config.js');
 var Twitter = new twit(config);
 
 //Retweet Bot ----------------------------------
