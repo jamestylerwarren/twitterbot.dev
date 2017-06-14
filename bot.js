f005ff9916306575ecfcf4df080d03d7369aac84
Tyler Warren <warrejt@yahoo.com>
2017-06-13 22:15:35 -0500

    retweet is logging both error and response - trying to fix

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@
 var twit = require('twit');
 var config = require('./config.js');
 var Twitter = new twit(config);
+
 var params = {
     q: '#popquote OR #greggpopovich OR #PopQuotes OR #PopQuotes OR #GreggPopovich OR #inpopwetrust OR #InPopWeTrust OR #WisdomOfCoachPop OR #PopForPrez OR #PresidentPop OR #CoachPoppa OR #ILikeItWhenYouCallMeCoachPoppa OR #CoachPoppa OR #PopForPresident OR #PopBeingPop OR #Pop2020 OR #PopGonnaPop OR #PopForever OR #PopForEver OR #Popovichkerr2020 OR #PopovichKerr2020', 
     result_type: 'recent',
@@ -10,28 +11,26 @@ var params = {
 
 //Retweet Bot ----------------------------------
 var retweet = function(){
+
     Twitter.get('search/tweets', params, function(err, data) {
       	// if there no errors
         if (!err) {
           // grab ID of tweet to retweet
             var retweetId = data.statuses[0].id_str;
             // Tell TWITTER to retweet
-            Twitter.post('statuses/retweet/:id', {
-                id: retweetId
-            }, function(err, response) {
-                if (response) {
-                    console.log('Retweeted!!!');
-                }
-                // if there was an error while tweeting
+            Twitter.post('statuses/retweet/:id', {id: retweetId}, function(err, response) {
                 if (err) {
                     console.log('Something went wrong while RETWEETING... Duplication maybe...');
-                }
+                    console.log(err);
+                }; 
+                if (response) {
+                    console.log('Retweeted');
+                    console.log(response);
+                };
             });
-        }
-        // if unable to Search a tweet
-        else {
+        } else {
           console.log('Something went wrong while SEARCHING...');
-        }
+        };
     });
 }
 // grab & retweet as soon as program is running...
